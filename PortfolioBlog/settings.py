aec10426737b32e1fd1d7a352388938d9163afef
PatKeenan <patkeenan.dev@gmail.com>
2021-02-11 15:06:13 -0500

    need to debug server 500 error

--- PortfolioBlog/settings.py
+++ PortfolioBlog/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["django-portfolio-blog.herokuapp.com", "patkeenan.dev"]
+ALLOWED_HOSTS = ["django-portfolio-blog.herokuapp.com", "patkeenan.dev", "www.patkeenan.dev"]
 
 
 # Application definition
